fd094f6a37c320b3e82f769f351090eb034cf7a7
vi5halsingh <vishalsinghlodhi16@gmail.com>
2026-07-12 21:29:32 +0530

    GCD or HCF or two numbers

---
+++ simple math/GCD.py
@@ -0,0 +1,17 @@
+def GCD(n1 , n2):
+    minLengh = min(n1,n2)
+    gcd = 0 
+    i = 1
+    while(i < minLengh):
+        rn1 =  n1 % i 
+        rn2 = n2 % i 
+        if rn1 == 0 and rn2 == 0 :
+            if gcd < i :
+                gcd = i
+        i = i + 1
+    print(gcd)
+GCD(20,15)
+
+
+
+
