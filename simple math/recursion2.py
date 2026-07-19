27fb4fac45d4d9a5b90082b9fe9db3159e1666a1
vi5halsingh <vishalsinghlodhi16@gmail.com>
2026-07-19 16:36:02 +0530

    Is stign panlindorm using recusion

--- simple math/recursion2.py
+++ simple math/recursion2.py
@@ -17,14 +17,28 @@ class recursion :
         arr[i] = arr[len(arr)- i - 1] 
         arr[len(arr)-1 - i] = temp
         return self.reverseArrayONeVariable(arr, i = i + 1)
+    def isStringPalindrom(self , str , i):
+        if i >= len(str) // 2 :
+            return True
+        
+        if str[i] != str[len(str) - i - 1 ] :
+            return False
+
+        return self.isStringPalindrom( str , i = i + 1) 
+
 
 obj = recursion()
-arr = [5,4,3,2,1]
-l = 0
-r = len(arr) - 1
-ans = obj.reverseArrayONeVariable(arr , 0)
-print(ans)
+# arr = [5,4,3,2,1]
+# l = 0
+# r = len(arr) - 1
+# ans = obj.reverseArrayONeVariable(arr , 0)
+# print(ans)
 
+
+if obj.isStringPalindrom("ABCDCBe", 0) :
+    print(f"string is palindrom")
+else :
+    print("not a palindrom string")
 
 
 # TC : o(n)
