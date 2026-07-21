f850ab1f350f7b49bc8baaf637b6261d570a5e89
vi5halsingh <vishalsinghlodhi16@gmail.com>
2026-07-21 20:25:52 +0530

    return all the sub sequence equal to the given sum

--- simple math/recursion4.py
+++ simple math/recursion4.py
@@ -13,9 +13,29 @@ class recusion :
         
         self.subSequentOF(arr , i + 1 , n , arrNew)
 
+    def subSequentEqualToSum(self , arr , i , n, arrNew , s , sum):
+
+        if i == n :
+            if s == sum :
+                print(arrNew)
+            return
+        
+        arrNew.append(arr[i])
+
+        s = s + arr[i]
+        
+        self.subSequentEqualToSum(arr , i + 1 , n, arrNew ,  s , sum)
+
+        arrNew.pop()
+
+        s = s - arr[i]
+        
+        self.subSequentEqualToSum(arr , i + 1 , n , arrNew ,  s , sum)
+
+
 rec = recusion()
 arrNew = []
-rec.subSequentOF([3,1,2] , 0 , 3 , arrNew)
+rec.subSequentEqualToSum([1, 2 , 1] , 0 , 3 , arrNew , 0 , 2)
 
 # TC  : O(2 ** n)
 # SC : O(n)
